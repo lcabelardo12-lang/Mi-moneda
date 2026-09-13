c2d7bdbb9160b48d2c325ada46d4f40816261b06
lcabelardo12-lang <lcabelardo12@gmail.com>
2026-09-13 10:58:59 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,9 @@
-var CACHE_NAME='mi-moneda-v1';
+var CACHE_NAME='mi-moneda-v2';
 var urls=['./','manifest.json'];
 self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(urls)}))});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.filter(function(k){return k!==CACHE_NAME}).map(function(k){return caches.delete(k)}))}))});
 self.addEventListener('fetch',function(e){
   if(e.request.url.indexOf('script.google.com')>=0){e.respondWith(fetch(e.request));return}
+  if(e.request.url.indexOf('logo.png')>=0){e.respondWith(fetch(e.request).then(function(r){var c=r.clone();caches.open(CACHE_NAME).then(function(ch){ch.put(e.request,c)});return r}).catch(function(){return caches.match(e.request)}));return}
   e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
 });
